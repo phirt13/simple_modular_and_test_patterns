97437710ee3ff1ae116525d5e88263a5159d9b59
Philip A. Hirt <phirt13@gmail.com>
2016-04-05 09:58:20 -0700

    finished bonus point 1

--- alex_hirt/lib/greet.js
+++ alex_hirt/lib/greet.js
@@ -1,9 +1,14 @@
-//ask about clarification about setting const hello = module.exports = exports = {....}
 module.exports = exports = {
     greet : function(name) {
       return 'hello ' + name;
     }
 };
+
+(function greetNode() {
+  if(process.argv[2]) {
+    console.log('hello ' + process.argv[2]);
+  }
+}());
 
 
 
