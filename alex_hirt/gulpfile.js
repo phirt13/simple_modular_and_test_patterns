84346c0c74160a8d178599e8212ed648a88d5041
Philip A. Hirt <phirt13@gmail.com>
2016-04-05 23:29:39 -0700

    finished gulpfile.js and added package.json but no rules in gulp

---
+++ alex_hirt/gulpfile.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+const mocha = require('gulp-mocha');
+
+gulp.task('lint:test', function() {
+  return gulp.src(['./test/**/*test.js'])
+    .pipe(eslint({
+      rules: {
+      },
+      envs: [
+        'mocha',
+        'es6'
+      ]
+    }))
+    .pipe(eslint.format());
+});
+
+var noTestScripts = ['./lib/**/*.js', 'gulpfile.js'];
+
+gulp.task('lint', function() {
+  return gulp.src(noTestScripts)
+    .pipe(eslint({
+      envs: [
+      'es6'
+      ]
+    }))
+    .pipe(eslint.format());
+});
+
+var allScripts = ['./lib/**/*.js', './test/**/*test.js', 'gulpfile.js'];
+
+gulp.task('lint:watch', function() {
+  gulp.watch(allScripts, ['lint:all']);
+});
+
+gulp.task('mocha', function() {
+  return gulp.src(['./test/**/*test.js'])
+    .pipe(mocha({'reporter' : 'nyan'}));
+});
+
+gulp.task('lint:all', ['lint:test', 'lint']);
+gulp.task('do-almost-everything', ['lint:test', 'lint', 'mocha'])
+gulp.task('default', ['do-almost-everything']);
